98e8e3e18a9f54c59e9aad4a5eae5cf2d200d4de
Saiban0 <bartched@outlook.fr>
2024-04-18 15:32:07 +0200

    imported gnl

---
+++ get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 14:44:07 by bchedru           #+#    #+#             */
+/*   Updated: 2024/04/11 18:41:49 by bchedru          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
+
+char	*get_next_line(int fd);
+int		ft_strlen(char *str);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strcpy(char *dst, char *src);
+int		ft_find_n(char *str);
+char	*ft_calloc(int nmemb, int size);
+char	*ft_read2buff(char *line, char *buffer, int fd);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 14:41:10 by bchedru           #+#    #+#             */
+/*   Updated: 2024/04/11 18:42:02 by bchedru          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	res = malloc((ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1));
+	if (!res)
+		return (NULL);
+	while (s1[j])
+	{
+		res[i] = s1[j];
+		j++;
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		res[i] = s2[j];
+		j++;
+		i++;
+	}
+	res[i] = '\0';
+	free((char *)s1);
+	return (res);
+}
+
+char	*ft_strcpy(char *dst, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+int	ft_find_n(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	*ft_calloc(int nmemb, int size)
+{
+	char	*res;
+	int		i;
+
+	i = 0;
+	res = (char *)malloc(nmemb * size);
+	if (!res)
+		return (NULL);
+	while (i < size)
+	{
+		res[i] = '\0';
+		i++;
+	}
+	return (res);
+}
