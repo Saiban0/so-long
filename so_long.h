8a5b40f5f836b9897f29ffebb5e91f3aed997adc
Saiban0 <bartched@outlook.fr>
2024-05-15 17:43:58 +0200

    added structure skeleton

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:05:32 by bchedru           #+#    #+#             */
-/*   Updated: 2024/04/18 15:29:00 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/05/15 16:25:20 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
-#include "get_next_line.h"
+#include "libft/libft.h"
+// #include "get_next_line.h"
+
+typedef struct	s_so_long
+{
+	
+}				t_so_long;
+
 
 #endif
