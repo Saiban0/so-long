b2bc25e0ccb7d56a9d4813ddb0432d1a7ae3fea7
Saiban0 <bartched@outlook.fr>
2024-06-12 19:31:59 +0200

    +| updated .h with current version

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:05:32 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/11 12:48:20 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/06/12 19:25:42 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,37 @@
 # include "../lib/libft/libft.h"
 # include "MLX42.h"
 
-typedef struct	s_so_long
+typedef struct s_so_long
 {
 	mlx_t	*mlx_ptr;
 	char	**map;
 	int		map_height;
 	int		map_width;
+	int		player_x;
+	int		player_y;
+	mlx_image_t	*wall_image;
+	mlx_image_t	*player_image;
+	mlx_image_t	*exit_image;
+	mlx_image_t	*bite_image;
+	mlx_image_t	*floor_image;
 }				t_so_long;
-
 
 /*   Parsing   */
 void	check_path(char	*path, t_so_long *game);
 void	open_file(t_so_long *game, char *map_path);
 int		check_length(char	*line, size_t width);
 int		parse(int depth, t_so_long *game, int fd);
+int		check_characters(t_so_long	*game);
+int		check_surrounded(t_so_long	*game);
 
 /*   Error Management   */
 void	safe_exit(char	*error_msg, t_so_long	*game);
-void	free_matrix(char **argv);
+void	free_matrix(char **matrix);
 
+/*  Images init  */
+void	textures_init(t_so_long *game);
+void	texture_placement(t_so_long *game);
 
+/*  Movements  */
 
 #endif
