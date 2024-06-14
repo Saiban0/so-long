94b1628c6f30b7fae186ee44cfc896dad64bbe50
Saiban0 <bartched@outlook.fr>
2024-06-14 20:19:16 +0200

    ~|Switched to t_coord

--- srcs/init_image.c
+++ srcs/init_image.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 18:21:00 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/13 19:47:46 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/06/14 20:18:52 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,48 +33,50 @@ void	textures_init(t_so_long *game)
 	mlx_delete_texture(temp);
 }
 
-// static void	player_image_placement(t_so_long *game, int i, int j)
-// {
-// 	mlx_image_to_window(game->mlx_ptr,
-// 	game->player_image, j * 32 , i * 32);
-// 	// game->player_image->instances->z = 1;
-// }
-
-// static void	floor_image_placement(t_so_long *game, int i, int j)
-// {
-// 	mlx_image_to_window(game->mlx_ptr,
-// 	game->floor_image, j * 32 , i * 32);
-// 	// game->floor_image->instances->z = 0;
-// }
+void	load_image(mlx_t *mlx, t_coord coord, mlx_image_t *image, bool enabled)
+{
+	mlx_image_to_window(mlx, image, coord.y * 32, coord.x * 32);
+	mlx_set_instance_depth(&image->instances[image->count - 1], coord.z);
+	image->instances->enabled = enabled;
+}
 
 void	images_placement(t_so_long *game)
 {
-	int	i;
-	int	j;
+	t_coord	co;
 
-	i = 0;
-	while (game->map[i])
+	co.x = 0;
+	while (game->map[co.x])
 	{
-		j = 0;
-		while (game->map[i][j])
+		co.y = 0;
+		while (game->map[co.x][co.y])
 		{
-			if (game->map[i][j] == '1')
-				mlx_image_to_window(game->mlx_ptr,
-					game->wall_image, j * 32, i * 32);
-			if (game->map[i][j] == '0')
-				mlx_image_to_window(game->mlx_ptr,
-					game->floor_image, j * 32, i * 32);
-			if (game->map[i][j] == 'P')
-				mlx_image_to_window(game->mlx_ptr,
-					game->player_image, j * 32, i * 32);
-			if (game->map[i][j] == 'E')
-				mlx_image_to_window(game->mlx_ptr,
-					game->exit_image, j * 32, i * 32);
-			if (game->map[i][j] == 'C')
-				mlx_image_to_window(game->mlx_ptr,
-					game->collectible_image, j * 32, i * 32);
-			j++;
+			if (game->map[co.x][co.y] == '1')
+			{
+				co.z = 5;
+				load_image(game->mlx_ptr, co,game->wall_image,1);
+			}
+			if (game->map[co.x][co.y] == '0')
+			{
+				co.z = 0;
+				load_image(game->mlx_ptr, co,game->floor_image,1);
+			}
+			if (game->map[co.x][co.y] == 'P')
+			{
+				co.z = 10;
+				load_image(game->mlx_ptr, co,game->player_image,1);
+			}
+			if (game->map[co.x][co.y] == 'E')
+			{
+				co.z = 5;
+				load_image(game->mlx_ptr, co,game->exit_image,1);
+			}
+			if (game->map[co.x][co.y] == 'C')
+			{
+				co.z = 5;
+				load_image(game->mlx_ptr, co,game->collectible_image,1);
+			}
+			co.y++;
 		}
-		i++;
+		co.x++;
 	}
 }
