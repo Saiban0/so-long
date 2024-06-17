6cdcd4a56671a57c98b7d2ea790c9d2ce5c0daef
Saiban0 <bartched@outlook.fr>
2024-06-17 19:19:59 +0200

    +| added ticks and monsters into .h

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:05:32 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/17 15:25:26 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/06/17 19:16:37 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,33 @@ typedef struct s_coord
 	int	z;
 }			t_coord;
 
+typedef struct s_monster
+{
+	t_coord		coords;
+	char		*direction;
+	mlx_image_t	*image;
+}			t_monster;
+
 typedef struct s_so_long
 {
-	mlx_t		*mlx_ptr;
-	char		**map;
-	int			map_height;
-	int			map_width;
-	int			movement_count;
-	int			collectibles;
-	int			max_collectibles;
-	int			player_health;
-	bool		collected_all;
-	t_coord		player_coords;
-	mlx_image_t	*wall_image;
-	mlx_image_t	*player_image;
-	mlx_image_t	*exit_image;
-	mlx_image_t	*collectible_image;
-	mlx_image_t	*floor_image;
+	mlx_t				*mlx_ptr;
+	char				**map;
+	int					map_height;
+	int					map_width;
+	int					movement_count;
+	int					collectibles;
+	int					max_collectibles;
+	int					player_health;
+	bool				collected_all;
+	t_coord				player_coords;
+	t_monster			*monster_array;
+	mlx_image_t			*wall_image;
+	mlx_image_t			*player_image;
+	mlx_image_t			*exit_image;
+	mlx_image_t			*collectible_image;
+	mlx_image_t			*floor_image;
+	mlx_image_t			*monster_image;
+	unsigned long long	ticks;
 }				t_so_long;
 
 # define SO_LONG_UNIT 32
@@ -57,6 +67,7 @@ int		check_length(char	*line, size_t width);
 int		parse(int depth, t_so_long *game, int fd);
 int		check_characters(t_so_long	*game);
 int		check_surrounded(t_so_long	*game);
+int		check_special_character_occurences(t_so_long *game);
 void	initialize_starting_variables(t_so_long *game);
 
 /*   Error Management   */
@@ -65,20 +76,27 @@ void	free_matrix(char **matrix);
 
 /*  Images init  */
 void	textures_init(t_so_long *game);
+void	load_image(mlx_t *mlx, t_coord coord, mlx_image_t *image, bool enabled);
 void	select_image_load(t_so_long *game, t_coord coords);
 void	images_placement(t_so_long *game);
 
-/*  Movements  */
+/*  Player movements  */
 void	player_move_up(t_so_long *game);
 void	player_move_down(t_so_long *game);
 void	player_move_left(t_so_long *game);
 void	player_move_right(t_so_long *game);
 
+/*  Monsters  */
+void	move_monsters(t_so_long *game, t_monster *array);
+void	initialize_monster_array(t_so_long *game);
+
 /*  Game loop  */
 void	game_loop(t_so_long *game);
 
-/*  Debug   */
+/*  Utils   */
 void	print_map(t_so_long *game);
 void	collectible_collision(t_so_long *game);
+void	update_map(t_so_long *game, t_coord coords);
+
 
 #endif
