bf5dee0b3e62136e331deff73788fae9fcf5f995
Saiban0 <bartched@outlook.fr>
2024-06-17 19:22:41 +0200

    +| optimized collectible colletion, added death and tick-tied enemy movement

--- srcs/game_loop.c
+++ srcs/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 20:11:31 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/17 15:15:25 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/06/17 18:31:33 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,26 @@
 
 void	game_loop(t_so_long *game)
 {
+	collectible_collision(game);
 	if (game->collectibles == game->max_collectibles)
 		game->collected_all = 1;
+	if (game->player_health < 1)
+		safe_exit("YOU DIED\n", game);
+	if (game->ticks % 60 == 0)
+		move_monsters(game, game->monster_array);
+	game->ticks++;
 }
 
 void	collectible_collision(t_so_long *game)
 {
 	t_coord	temp;
 
-	temp.x = game->player_coords.x;
-	temp.y = game->player_coords.y;
-	temp.z = 10;
-	select_image_load(game, temp);
-	game->collectibles++;
+	if(game->map[game->player_coords.x][game->player_coords.y] == 'C')
+	{
+		temp.x = game->player_coords.x;
+		temp.y = game->player_coords.y;
+		temp.z = 6;
+		load_image(game->mlx_ptr, temp, game->floor_image, 1);
+		game->collectibles++;
+	}
 }
