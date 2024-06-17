abd0d8745487b8101c2fe8cb1e8b1fc40b37fbac
Saiban0 <bartched@outlook.fr>
2024-06-17 19:24:00 +0200

    ~| added monster collision and optimized collectible collisions

--- srcs/player_movements.c
+++ srcs/player_movements.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 19:25:02 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/17 15:05:46 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/06/17 18:29:56 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,12 @@ static	int	is_valid_move(t_so_long *game, int mvt_x, int mvt_y)
 	if (game->map[new_x][new_y] == '1' || (game->map[new_x][new_y] == 'E'
 				&& game->collected_all != 1))
 		return (0);
+	if (game->map[new_x][new_y] == 'M')
+	{
+		game->player_health--;
+		ft_printf("You got damaged, new health = %d\n", game->player_health);
+		return (0);
+	}
 	return (1);
 }
 
@@ -32,13 +38,9 @@ void	player_move_up(t_so_long *game)
 {
 	if (is_valid_move(game, -1, 0) == 1)
 	{
-		game->map[game->player_coords.x][game->player_coords.y] = '0';
 		game->player_coords.x -= 1;
-		if(game->map[game->player_coords.x][game->player_coords.y] == 'C')
-			collectible_collision(game);
 		if(game->map[game->player_coords.x][game->player_coords.y] == 'E')
 			safe_exit("You won!", game);
-		game->map[game->player_coords.x][game->player_coords.y] = 'P';
 		game->player_image->instances->y -= SO_LONG_UNIT;
 		game->movement_count++;
 		ft_printf("Movements = %d\n", game->movement_count);
@@ -49,13 +51,9 @@ void	player_move_down(t_so_long *game)
 {
 	if (is_valid_move(game, 1, 0) == 1)
 	{
-		game->map[game->player_coords.x][game->player_coords.y] = '0';
 		game->player_coords.x += 1;
-		if(game->map[game->player_coords.x][game->player_coords.y] == 'C')
-			collectible_collision(game);
 		if(game->map[game->player_coords.x][game->player_coords.y] == 'E')
 			safe_exit("You won!", game);
-		game->map[game->player_coords.x][game->player_coords.y] = 'P';
 		game->player_image->instances->y += SO_LONG_UNIT;
 		game->movement_count++;
 		ft_printf("Movements = %d\n", game->movement_count);
@@ -66,13 +64,9 @@ void	player_move_left(t_so_long *game)
 {
 	if (is_valid_move(game, 0, -1) == 1)
 	{
-		game->map[game->player_coords.x][game->player_coords.y] = '0';
 		game->player_coords.y -= 1;
-		if(game->map[game->player_coords.x][game->player_coords.y] == 'C')
-			collectible_collision(game);
 		if(game->map[game->player_coords.x][game->player_coords.y] == 'E')
 			safe_exit("You won!", game);
-		game->map[game->player_coords.x][game->player_coords.y] = 'P';
 		game->player_image->instances->x -= SO_LONG_UNIT;
 		game->movement_count++;
 		ft_printf("Movements = %d\n", game->movement_count);
@@ -83,13 +77,9 @@ void	player_move_right(t_so_long *game)
 {
 	if (is_valid_move(game, 0, 1) == 1)
 	{
-		game->map[game->player_coords.x][game->player_coords.y] = '0';
 		game->player_coords.y += 1;
-		if(game->map[game->player_coords.x][game->player_coords.y] == 'C')
-			collectible_collision(game);
 		if(game->map[game->player_coords.x][game->player_coords.y] == 'E')
 			safe_exit("You won!", game);
-		game->map[game->player_coords.x][game->player_coords.y] = 'P';
 		game->player_image->instances->x += SO_LONG_UNIT;
 		game->movement_count++;
 		ft_printf("Movements = %d\n", game->movement_count);
