8980d16c781e104d8fab731a25728c24b7652b8b
Saiban0 <bartched@outlook.fr>
2024-06-19 18:59:23 +0200

    ~| optimized text display

--- srcs/display_text.c
+++ srcs/display_text.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 17:51:56 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/18 20:25:53 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/06/19 16:46:40 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 
 void	display_movement(t_so_long *game)
 {
-	char	*str;
 	char	*nbr;
 
+	if (!game->moves_str)
+		game->moves_str = mlx_put_string(game->mlx_ptr, " ", -10, -10);
 	mlx_delete_image(game->mlx_ptr, game->moves_str);
-	str = " Movements";
 	nbr = ft_itoa(game->movement_count);
-	ft_strcat(nbr, str);
+	ft_strcat(nbr, " Movements");
 	game->moves_str = mlx_put_string(game->mlx_ptr, nbr, 16, 2);
 	free(nbr);
 }
 
 void	display_lives(t_so_long *game)
 {
-	char	*str;
 	char	*nbr;
 
+	if (!game->lives_str)
+		game->lives_str = mlx_put_string(game->mlx_ptr, " ", -10, -10);
 	mlx_delete_image(game->mlx_ptr, game->lives_str);
-	str = " Lives";
 	nbr = ft_itoa(game->player_health);
-	ft_strcat(nbr, str);
+	ft_strcat(nbr, " Lives");
 	game->lives_str = mlx_put_string(game->mlx_ptr, nbr, 16, 18);
 	free(nbr);
 }
